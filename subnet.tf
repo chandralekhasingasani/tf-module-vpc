efbb992f87ebe2a84bb18358c69b1647ec6aa31d
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-06-06 08:40:20 +0530

    test

--- subnet.tf
+++ subnet.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "main" {
-  count      = length(var.AZ)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.SUBNET_CIDR_BLOCK, count.index)
+  count             = length(var.SUBNET_CIDR_BLOCK)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(var.SUBNET_CIDR_BLOCK, count.index)
+  availability_zone = element(var.AZ, count.index)
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
