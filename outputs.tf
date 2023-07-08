675966be6fa693345a31769fe06b3f5baf1dad56
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-08 23:26:22 +0530

    test

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,17 @@ output "VPC_CIDR" {
 output "SUBNET_IDS" {
   value = aws_subnet.main.*.id
 }
+
+output "PRIVATE_HOSTED_ZONE_ID" {
+  value = data.aws_route53_zone.selected.id
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "roboshop.internal"
+  private_zone = true
+}
+
+resource "aws_route53_zone_association" "secondary" {
+  zone_id = data.aws_route53_zone.selected.id
+  vpc_id  = aws_vpc.main.id
+}
