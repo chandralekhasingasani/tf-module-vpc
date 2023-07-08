0db58499f01a0200046f4f5910d5312e26e013bd
chandralekhasingasani <chandralekha.s@nutanix.com>
2023-07-08 16:35:56 +0530

    test

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 resource "aws_vpc" "main" {
   cidr_block       = var.CIDR_BLOCK
-
+  enable_dns_hostnames = true
+  enable_dns_support   = true
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"
   }
 }
-
